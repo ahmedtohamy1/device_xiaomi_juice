7510fb5729a841b6a3061f27f1fa12bdf448b9c8
Nick <36119499+NickJared@users.noreply.github.com>
2021-04-19 10:40:00 -0400

    Update BoardConfig.mk
    Trying to help fix dt2w by copying process in sm6125

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,6 +62,9 @@ TARGET_USES_HWC2 := true
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
+#DT2W
+TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
+
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
