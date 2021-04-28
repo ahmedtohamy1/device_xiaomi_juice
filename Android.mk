4c7d0061b3c6b0353a7271cb2fd14fb6fee09a1b
Nick <nick.jared@outlook.cl>
2021-04-28 13:50:45 -0400

    LA.UM.9.15.1.r1-02800-KAMORTA.0

--- Android.mk
+++ Android.mk
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
+||||||| 9986518 (LA.UM.9.15.1.r1-02800-KAMORTA.0)
+<<<<<<< HEAD
+=======
+#
+>>>>>>> parent of 9986518 (LA.UM.9.15.1.r1-02800-KAMORTA.0)
 # Copyright (C) 2021 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
