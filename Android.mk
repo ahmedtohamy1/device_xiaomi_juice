019849819495ca734b4c9f099fc847334fab8d05
Nick <36119499+NickJared@users.noreply.github.com>
2021-04-28 11:30:56 -0400

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Copyright (C) 2021 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
@@ -13,5 +12,3 @@ include $(CLEAR_VARS)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 endif
-
->>>>>>> a098720c3ad02fd43ecc7aedb3ed067156526540
